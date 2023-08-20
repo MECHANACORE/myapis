0f3c4ab5681dd00bbb896dfaf1cee5a0b650ff99
MECHANACORE <50303864+MECHANACORE@users.noreply.github.com>
2023-08-20 11:49:40 -0400

    Update
    put back api/v1/vegetables

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
     res.send("hello world");
 })
 
-app.use("https://amintuapi.onrender.com", vegetableRoutes);
+app.use("/api/v1/vegetables", vegetableRoutes);
 
 app.listen(port, () => console.log('running active port'));
